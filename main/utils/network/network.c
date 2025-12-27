403168ba5ee7e1a3b20a269f0db82e548d2380d1
CenturySturgeon <gfsolucionesintegradas@gmail.com>
2025-12-27 09:28:09 -0600

    Added ping uitl func, debug logs

--- main/utils/network/network.c
+++ main/utils/network/network.c
@@ -1,10 +1,85 @@
+#include <stdio.h>
+#include <string.h>
+
 #include "lwip/sockets.h"
 #include "lwip/netdb.h"
 #include "esp_log.h"
+#include "ping/ping_sock.h"
 
 #include "network.h"
 
 static const char *TAG = "NETWORK UTILS";
+
+// Callback for when a ping response is received or times out
+static void cmd_ping_on_ping_success(esp_ping_handle_t hdl, void *args)
+{
+    uint8_t ttl;
+    uint16_t seqno;
+    uint32_t elapsed_time, recv_len;
+    ip_addr_t target_addr;
+
+    esp_ping_get_profile(hdl, ESP_PING_PROF_SEQNO, &seqno, sizeof(seqno));
+    esp_ping_get_profile(hdl, ESP_PING_PROF_TTL, &ttl, sizeof(ttl));
+    esp_ping_get_profile(hdl, ESP_PING_PROF_IPADDR, &target_addr, sizeof(target_addr));
+    esp_ping_get_profile(hdl, ESP_PING_PROF_SIZE, &recv_len, sizeof(recv_len));
+    esp_ping_get_profile(hdl, ESP_PING_PROF_TIMEGAP, &elapsed_time, sizeof(elapsed_time));
+
+    ESP_LOGD(TAG, "%d bytes from %s icmp_seq=%d ttl=%d time=%d ms\n", recv_len, ip4addr_ntoa(&target_addr.u_addr.ip4), seqno, ttl, elapsed_time);
+}
+
+static void cmd_ping_on_ping_timeout(esp_ping_handle_t hdl, void *args)
+{
+    uint16_t seqno;
+    esp_ping_get_profile(hdl, ESP_PING_PROF_SEQNO, &seqno, sizeof(seqno));
+    ESP_LOGD(TAG, "From ping: timeout icmp_seq=%d\n", seqno);
+}
+
+static void cmd_ping_on_ping_end(esp_ping_handle_t hdl, void *args)
+{
+    ESP_LOGI(TAG, "Ping session finished. Cleaning up resources...");
+
+    // Delete session, free memory
+    esp_ping_delete_session(hdl);
+}
+
+esp_err_t send_ping(const char *ip_str)
+{
+    esp_ping_config_t ping_config = ESP_PING_DEFAULT_CONFIG();
+
+    ip_addr_t target_addr;
+    memset(&target_addr, 0, sizeof(target_addr));
+
+    // Parse the IP string
+    if (ipaddr_aton(ip_str, &target_addr) == 0)
+    {
+        return ESP_ERR_INVALID_ARG;
+    }
+
+    ping_config.target_addr = target_addr;
+    ping_config.count = 5; // 0 means infinite ping
+    esp_ping_callbacks_t cbs = {
+        .on_ping_success = cmd_ping_on_ping_success,
+        .on_ping_timeout = cmd_ping_on_ping_timeout,
+        .on_ping_end = cmd_ping_on_ping_end,
+        .cb_args = NULL};
+
+    esp_ping_handle_t ping_handle;
+    esp_err_t err = esp_ping_new_session(&ping_config, &cbs, &ping_handle);
+
+    if (err == ESP_OK)
+    {
+        err = esp_ping_start(ping_handle);
+        if (err != ESP_OK)
+        {
+            esp_ping_delete_session(ping_handle);
+            return err;
+        }
+
+        return ESP_OK;
+    }
+
+    return ESP_FAIL;
+}
 
 esp_err_t send_wol_packet(const char *mac_str, const char *secure_str, const char *broadcast_str)
 {
--- main/utils/network/network.h
+++ main/utils/network/network.h
@@ -4,6 +4,7 @@
 
 #include "esp_err.h"
 
+esp_err_t send_ping(const char *ip_str);
 esp_err_t send_wol_packet(const char *mac_str, const char *secure_str, const char *broadcast_str);
 
 #endif // NETWORK_UTILS_H
